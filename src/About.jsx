b95d24ea15d0974e78e7c71e5ab03d5209f54e3b
ananiya muluken <ananiyakok@gmail.com>
2024-09-17 09:37:19 +0300

    home/about us page

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <>
     <div>About</div>
-    <Link to="/">Home</Link>
+    <Link to="/">Home page</Link>
     </>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div>Home</div>
-    <Link to="/about">About</Link>
+    <Link to="/about">About us page</Link>
     </>
   )
 }
