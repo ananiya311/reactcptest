0e6f8b54cc46f121d9f338434ba395210cb4e9bf
ananiya muluken <ananiyakok@gmail.com>
2024-09-22 13:07:46 +0300

    asdfasdf

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <>
-    <div>Home, welcome test</div>
+    <div>Home, welcome test test test</div>
     <Link to="/about">About us page</Link>
     </>
   )
